a88d60bce5d25098bbe9889f4570f2c413fa96b9
AnkitaAdvitot <110318342+AnkitaAdvitot@users.noreply.github.com>
2025-04-08 11:06:38 +0530

    updated

---
+++ array/programs/FrequencyWithoutHashMap.java
@@ -0,0 +1,27 @@
+package array.programs;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
+class FrequencyWithoutHashMap{
+    static int[] arr=new int[26];
+    static void freq(String s){
+        for(char ch:s.toCharArray()){
+            arr[ch-'a']++;
+        }
+        System.out.println("Frequency of "+s);
+        for(int i=0;i<26;i++){
+            if(arr[i]>0){
+                System.out.println((char)( i+'a')+" - "+arr[i]);
+            }
+        } 
+    }
+   public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String ");
+        String s=sc.next();
+        freq(s);
+      
+   }
+}
